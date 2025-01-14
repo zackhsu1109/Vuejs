f90c195d6f05817bdba84860fc711b30ca25f1d8
Zack <zack149633218@gmail.com>
2025-01-14 10:23:34 +0800

    change logo

--- vue-app/src/components/NavbarComponent.vue
+++ vue-app/src/components/NavbarComponent.vue
@@ -7,9 +7,14 @@ const todoStore = useTodoStore()
 <template>
   <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#"> <img
+      <!--<a class="navbar-brand" href="#"> <img
           src="https://www.ispan.com.tw/_next/image/?url=%2Fimages%2FiSpan_Logo_slogan.png&w=1080&q=75" alt="Logo"
-          width="80" class="d-inline-block align-text-top"></a>
+          width="80" class="d-inline-block align-text-top"></a>-->
+
+      <a class="navbar-brand" href="#"> <img
+          src="https://live.staticflickr.com/65535/51358909913_5689806c1f_m.jpg" alt="Logo"
+          width="80" class="d-inline-block align-text-top"></a>    
+
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
         aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
